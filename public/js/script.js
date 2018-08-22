df158d7b2506b3fdfc9b16466105ee3eb50376c3
Ian Zablovschii <iz@insomniacdesign.com>
2018-08-22 14:42:24 +0300

    (fix) Remove default county

--- public/js/script.js
+++ public/js/script.js
@@ -143,11 +143,6 @@ $(document).ready(function() {
     }
   });
 
-  // Temporary solution in order to show county on first page load
-  setTimeout(function() {
-     $('#searchCounty').val('55027-WI027-WISCONSIN').trigger('change.select2');
-   }, 3000);
-
 });
 
 function temperatureTooltip(args) {
